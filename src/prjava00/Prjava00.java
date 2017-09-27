737d72ef912962140deabe8f1051683f14c56a15
laksas <laksas@jordi>
2017-09-27 19:16:06 +0200

    Versió 0.1 del projecte prjava00

---
+++ src/prjava00/Prjava00.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava00;
+
+/**
+ *
+ * @author laksas
+ */
+public class Prjava00 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola món");
+        System.out.println("Versió 0.1 del projecte prjava00");
+    }
+    
+}
